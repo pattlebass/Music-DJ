b1bc5966315471011e0c354493ca9af05a124c85
pattlebass <49322676+pattlebass@users.noreply.github.com>
2022-06-02 19:29:23 +0300

    Use button group for SoundDialog

--- scenes/SoundDialog.gd
+++ scenes/SoundDialog.gd
@@ -10,7 +10,7 @@ var column
 
 onready var button_container = $VBoxContainer/ScrollContainer/VBoxContainer
 onready var audio_player = $AudioStreamPlayer
-
+var button_group = ButtonGroup.new()
 
 func _ready():
 	if !OS.is_ok_left_and_cancel_right():
@@ -55,7 +55,6 @@ func _ready():
 			Variables.category_names[i],
 			"Tile"
 		)
-		#image.create(32, 32, false, Image.FORMAT_RGBA4444)
 		image.lock()
 		for v in 32:
 			for h in 32:
@@ -77,6 +76,7 @@ func _ready():
 			button_in_list.name = str(button_index)
 			button_in_list.focus_mode = Control.FOCUS_CLICK
 			button_in_list.toggle_mode = true
+			button_in_list.group = button_group
 			button_container.add_child(button_in_list)
 
 
@@ -116,12 +116,6 @@ func on_Button_selected(index, _genre_index, _sample_category):
 	pressed_button_index = index
 	genre_index = _genre_index
 	sample_category = _sample_category
-	
-	for i in button_container.get_children():
-		if i is Button:
-			if i.name == str(index):
-				continue
-			i.pressed = false
 
 
 func _on_OkButton_pressed():
@@ -165,7 +159,8 @@ func _on_CancelButton_pressed():
 
 func popup_hide(dim := true):
 	$VBoxContainer/ScrollContainer.scroll_vertical = 0
-	for i in button_container.get_children():
-		if i is Button:
-			i.pressed = false
+	
+	if button_group.get_pressed_button():
+		button_group.get_pressed_button().pressed = false
+	
 	.popup_hide()
