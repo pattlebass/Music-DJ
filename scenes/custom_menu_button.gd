bf658168d387e1734be23bfa9225425c1b2e06cd
Fabian S. <49322676+pattlebass@users.noreply.github.com>
2026-05-10 20:57:44 +0300

    Fix CustomPopupMenu positioning on mobile

--- scenes/custom_menu_button.gd
+++ scenes/custom_menu_button.gd
@@ -7,17 +7,16 @@ extends Button
 func _ready() -> void:
 	add_child(popup_menu)
 	popup_menu.container.pivot_offset_ratio = Vector2(1, 0)
-	#popup_menu.top_level = true
-	#popup_menu.hide()
 	
 	pressed.connect(_on_about_to_popup)
 
 
 func _on_about_to_popup() -> void:
 	popup_menu.popup2()
-	if OS.has_feature("pc"):
-		popup_menu.position.y = global_position.y + int(size.y)
+	popup_menu.position.y = global_position.y
 	popup_menu.position.x = global_position.x + int(size.x - popup_menu.size.x)
+	if OS.has_feature("pc"):
+		popup_menu.position.y += int(size.y)
 
 
 func get_popup() -> CustomPopupMenu:
